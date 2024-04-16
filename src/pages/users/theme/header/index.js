f21479365aeec08fe89b946760915c932e4647a4
doan <nvd04062002@gmail.com>
2024-04-16 11:47:28 +0700

    header

--- src/pages/users/theme/header/index.js
+++ src/pages/users/theme/header/index.js
@@ -1,7 +1,34 @@
-import { memo } from "react";
+import React from "react";
+import classNames from "classnames/bind";
+import styles from "./header.module.scss";
+import { CgProfile } from "react-icons/cg";
+import { Link } from "react-router-dom";
+import { formatter } from "../../../../untils/formater";
+const cx = classNames.bind(styles);
+function Header() {
+  return (
+    <div className={cx("header_top")}>
+      <div className={cx("container")}>
+        <div className={cx("row")}>
+          <div className={cx("col-6_top_left")}>
+            <ul>
+              <li>{`miễn phí ship đơn từ ${formatter(200000)}`}</li>
+            </ul>
+          </div>
+          <div className={cx("col-6_top_right")}>
+            <ul>
+              <li>
+                <Link to={""}>
+                  <CgProfile />
+                  <span>Đăng nhập</span>
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
 
-const Header = () => {
-  return <div className="container">Header</div>;
-};
-
-export default memo(Header);
+export default Header;
