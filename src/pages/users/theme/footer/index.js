e2addbb4cab0e9196117231bca09bd5cc32fa98a
doan <nvd04062002@gmail.com>
2024-04-19 14:25:50 +0700

    header+footer

--- src/pages/users/theme/footer/index.js
+++ src/pages/users/theme/footer/index.js
@@ -1,8 +1,71 @@
-import { memo } from "react";
+import react from "react";
+import styles from "./footer.module.scss";
+import classNames from "classnames/bind";
+import { Link } from "react-router-dom";
+import { AiOutlineFacebook } from "react-icons/ai";
 
-const Footer = () => {
-     return  <h1>Footer</h1>
-     
-};
+const cx = classNames.bind(styles);
+function Footer() {
+  return (
+    <footer className={cx("footer")}>
+      <div className={cx("container")}>
+        <div className={cx("row")}>
+          <div className={cx("col-lg-3")}>
+            <div className={cx("footer_about")}>
+              <h1 className={cx("footer_about_logo")}>Highland</h1>
+              <ul>
+                <li>Địa chỉ: 34 Phú Mộng Kim long</li>
+                <li>Phone: 0702324357 </li>
+                <li>Email: nvd04062002@gmail.com</li>
+              </ul>
+            </div>
+          </div>
+          <div className={cx("col-lg-6")}>
+            <div className={cx("footer_widget")}>
+              <h6>Cửa hàng</h6>
+              <ul>
+                <li>
+                  <Link to="">Liên hệ</Link>
+                </li>
+                <li>
+                  <Link to="">Thông tin về chúng tôi</Link>
+                </li>
+                <li>
+                  <Link to="">Sản phẩm kinh doanh</Link>
+                </li>
+              </ul>
+              <ul>
+                <li>
+                  <Link to="">Thông tin tài khoản</Link>
+                </li>
+                <li>
+                  <Link to="">giỏ hàng</Link>
+                </li>
+                <li>
+                  <Link to="">danh sách yêu thích</Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className={cx("col-lg-3")}>
+            <div className={cx("footer_widget")}>
+              <h6>Khuyến mãi & ưu đãi</h6>
+              <p>Đăng kí nhận thông tin tại đây</p>
+              <form action="#">
+                <div className="input-group">
+                  <input type="text" placeholder="Nhập email" />
+                  <button type="submit" className="button-submit">
+                    {" "}
+                    Đăng ký
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
 
-export default memo(Footer);
+export default Footer;
--- src/pages/users/theme/header/index.js
+++ src/pages/users/theme/header/index.js
@@ -1,19 +1,50 @@
-// import React, { useState } from "react";
+import React, { useState } from "react";
 import classNames from "classnames/bind";
 import styles from "./header.module.scss";
 import { CgProfile } from "react-icons/cg";
 import { AiOutlineShoppingCart } from "react-icons/ai";
 import { Link } from "react-router-dom";
 import { formatter } from "../../../../untils/formater";
-// import { ROUTERS } from "../../../../untils/router";
+import { ROUTERS } from "../../../../untils/router";
 const cx = classNames.bind(styles);
 function Header() {
-  // const [menus, setMenus] = useState([
-  //   {
-  //     name: "Trang Chủ",
-  //     path: ROUTERS.USER.HOME,
-  //   },
-  // ]);
+  const [menus, setMenus] = useState([
+    {
+      name: "Trang Chủ",
+      path: ROUTERS.USER.HOME,
+    },
+    {
+      name: "Cửa hàng",
+      path: ROUTERS.USER.PRODUCTS,
+    },
+    {
+      name: "Sản phẩm",
+      path: "",
+      isShowSubmenu: false,
+      child: [
+        {
+          name: "COFFEE",
+          path: "",
+        },
+        {
+          name: "Trà nhiệt đới",
+          path: "",
+        },
+        {
+          name: "Trà sữa",
+          path: "",
+        },
+      ],
+    },
+    {
+      name: "Bài viết",
+      path: "",
+    },
+    {
+      name: "Liên Hệ",
+      path: "",
+    },
+  ]);
   return (
     <>
       <div className={cx("header_top")}>
@@ -40,11 +71,30 @@ function Header() {
       <div className={cx("container")}>
         <div className={cx("row")}>
           <div className={cx("col-xl-3")}>
-            <div className="header_logo">
+            <div className={cx("header_logo")}>
               <h1>Highland</h1>
             </div>
           </div>
-          <div className={cx("col-xl-6")}>MENU</div>
+          <div className={cx("col-xl-6")}>
+            <nav className={cx("header_menu")}>
+              <ul>
+                {menus?.map((menu, menuKey) => (
+                  <li key={menuKey} className={menuKey === 0 ? "active" : ""}>
+                    <Link to={menu?.path}>{menu?.name}</Link>
+                    {menu.child && (
+                      <ul className={cx("header_menu_dropdow")}>
+                        {menu.child.map((childItem, childKey) => (
+                          <li key={`${menuKey}-${childKey}`}>
+                            <Link to={childItem.path}>{childItem.name}</Link>
+                          </li>
+                        ))}
+                      </ul>
+                    )}
+                  </li>
+                ))}
+              </ul>
+            </nav>
+          </div>
           <div className={cx("col-xl-3")}>
             <div className={cx("header_cart")}>
               <div className={cx("header_cart_price")}>
